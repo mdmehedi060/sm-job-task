3197de81a9e01353c37952e3c500f6e2e8caf26d
mdmehedi060 <mdmehedi060@gmail.com>
2024-04-23 20:17:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Shared/Footer";
+import Navbar from "./Shared/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
